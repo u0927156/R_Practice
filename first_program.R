fe415e8dfff82d4beb5057fd8722b37e7a643414
Spencer Peterson <u0927156@utah.edu>
2020-07-08 17:47:52 -0600

    Update first_program.R
    Added more basics

--- first_program.R
+++ first_program.R
@@ -1,7 +1,19 @@
 # This is my first R script. 
-# It adds some numbers together and creates a vector
+# It says hello world, adds some numbers together, and creates a vector
 # Author: Spencer Peterson
 # Date: 7/8/2020
 
 
+print('hello world')
+
 1+1
+
+r <- 6 
+
+r + 2
+
+x<-c(10, 8, 6, 4, 2, 0)
+x
+x*2 - 1 
+y<-(x + 10)/2
+y
